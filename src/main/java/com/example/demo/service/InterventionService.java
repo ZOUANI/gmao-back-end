d99c0dd2aca6889dfb11d5c1623770f75da51b20
Mehdi <elwafi.courrier99@gmail.com>
2021-05-09 22:52:00 +0000

    Fixed interventionService

--- src/main/java/com/example/demo/service/InterventionService.java
+++ src/main/java/com/example/demo/service/InterventionService.java
@@ -30,10 +30,10 @@ public class InterventionService {
     private MateraialInterventionService materaialInterventionService;
     @Autowired
     private ConseilsService conseilsService;
+    // @Autowired
+    // private InterventionVo interventionVo;
     @Autowired
-    private InterventionVo interventionVo;
-    @Autowired
-    private  EntityManager entityManager;
+    private EntityManager entityManager;
 
     public Intervention findByCode(String code) {
         return interventionDao.findByCode(code);
@@ -54,23 +54,26 @@ public class InterventionService {
     public List<Intervention> findAll() {
         return interventionDao.findAll();
     }
-    public List<Intervention> findByCriteria(InterventionVo interventionVo){
-        String query="SELECT o FROM Intervention o WHERE 1=1 ";
-        if(StringUtil.isNotEmpty(interventionVo.getCode()))
-            query+=" AND o.code LIKE '%"+interventionVo.getCode()+"%'";
-         if (interventionVo.getDateDeProbleme()!=null )
-            query+=" AND o.dateDeProbleme = '"+interventionVo.getDateDeProbleme()+"'";
-         if (interventionVo.getDateDebut()!=null )
-            query+=" AND o.dateDebut = '"+interventionVo.getDateDebut()+"'";
-         if (interventionVo.getDateFin()!=null );
-            query+=" AND o.dateFin = '"+interventionVo.getDateFin()+"'";
-        if(StringUtil.isNotEmpty(interventionVo.getDescription()))
-            query+=" AND o.description '%"+interventionVo.getDescription()+"%'";
-        if(StringUtil.isNotEmpty(interventionVo.getLibelle()))
-            query+=" AND o.libelle  '%"+interventionVo.getLibelle()+"%'";
+
+    public List<Intervention> findByCriteria(InterventionVo interventionVo) {
+        String query = "SELECT o FROM Intervention o WHERE 1=1 ";
+        if (StringUtil.isNotEmpty(interventionVo.getCode()))
+            query += " AND o.code LIKE '%" + interventionVo.getCode() + "%'";
+        if (interventionVo.getDateDeProbleme() != null)
+            query += " AND o.dateDeProbleme = '" + interventionVo.getDateDeProbleme() + "'";
+        if (interventionVo.getDateDebut() != null)
+            query += " AND o.dateDebut = '" + interventionVo.getDateDebut() + "'";
+        if (interventionVo.getDateFin() != null)
+
+            query += " AND o.dateFin = '" + interventionVo.getDateFin() + "'";
+        if (StringUtil.isNotEmpty(interventionVo.getDescription()))
+            query += " AND o.description '%" + interventionVo.getDescription() + "%'";
+        if (StringUtil.isNotEmpty(interventionVo.getLibelle()))
+            query += " AND o.libelle  '%" + interventionVo.getLibelle() + "%'";
 
         return entityManager.createQuery(query).getResultList();
     }
+
     @Transactional
     public int deleteByCode(String code) {
         return interventionDao.deleteByCode(code);
@@ -80,17 +83,18 @@ public class InterventionService {
     public int deleteByEtatIntervention(String etatIntervention) {
         return interventionDao.deleteByEtatIntervention(etatIntervention);
     }
-    public int update(String code,Intervention intervention){
-        Intervention interventions=findByCode(code);
-        if (interventions!=null){
-            if(intervention.getEtatIntervention()!=null)
-                 interventions.setEtatIntervention(intervention.getEtatIntervention());
-            if (intervention.getInterventionMembreEquipe()!=null)
+
+    public int update(String code, Intervention intervention) {
+        Intervention interventions = findByCode(code);
+        if (interventions != null) {
+            if (intervention.getEtatIntervention() != null)
+                interventions.setEtatIntervention(intervention.getEtatIntervention());
+            if (intervention.getInterventionMembreEquipe() != null)
                 interventions.setInterventionMembreEquipe(intervention.getInterventionMembreEquipe());
-            if (intervention.getMateraialInterventions()!=null)
+            if (intervention.getMateraialInterventions() != null)
                 interventions.setMateraialInterventions(intervention.getMateraialInterventions());
             interventions.setCode(intervention.getCode());
-            if (intervention.getConseils()!=null)
+            if (intervention.getConseils() != null)
                 interventions.setConseils(intervention.getConseils());
             interventions.setDateDebut(intervention.getDateDebut());
             interventions.setDateDeProbleme(intervention.getDateDeProbleme());
@@ -98,36 +102,35 @@ public class InterventionService {
             interventions.setDescription(intervention.getDescription());
             interventions.setLibelle(intervention.getLibelle());
             interventionDao.save(interventions);
-            return  1;
-        }
-        else
+            return 1;
+        } else
             return -2;
     }
+
     public int save(Intervention intervention) {
-        EtatIntervention etatIntervention = etatInterventionService.findByCode(intervention.getEtatIntervention().getCode());
-        if(etatIntervention==null)
+        EtatIntervention etatIntervention = etatInterventionService
+                .findByCode(intervention.getEtatIntervention().getCode());
+        if (etatIntervention == null)
             return -2;
 
-        if (findByCode(intervention.getCode())==null) {
+        if (findByCode(intervention.getCode()) == null) {
             intervention.setEtatIntervention(etatIntervention);
             interventionDao.save(intervention);
-            int b = 2, a = 2,c=2;
+            int b = 2, a = 2, c = 2;
             for (MateraialIntervention materaialIntervention : intervention.getMateraialInterventions()) {
                 materaialIntervention.setIntervention(intervention);
                 b = materaialInterventionService.save(materaialIntervention);
             }
             for (InterventionMembreEquipe interventionCollaborateur : intervention.getInterventionMembreEquipe()) {
-                a = interventionMembreEquipeService.save(interventionCollaborateur,intervention);
+                a = interventionMembreEquipeService.save(interventionCollaborateur, intervention);
             }
-            for(Conseils conseils : intervention.getConseils()){
-                c=conseilsService.save(intervention,conseils);
+            for (Conseils conseils : intervention.getConseils()) {
+                c = conseilsService.save(intervention, conseils);
             }
 
             return a + b + c;
-        }
-        else
+        } else
             return -1;
     }
-
 
 }
